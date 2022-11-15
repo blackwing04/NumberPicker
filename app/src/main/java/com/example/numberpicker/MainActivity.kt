9ed873441e9f924b38c4656e2b761960b4fade37
blackwing <hh4654075hh@gmail.com>
2022-11-15 19:51:57 +0800

    建立密碼鎖外觀及設定密碼吐司文字顯示選的密碼

---
+++ app/src/main/java/com/example/numberpicker/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.numberpicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fun setNp(np:NumberPicker){
+            np.minValue=0
+            np.maxValue=9
+        }
+        val np1 = findViewById<NumberPicker>(R.id.np1)
+        val np2 = findViewById<NumberPicker>(R.id.np2)
+        val np3 = findViewById<NumberPicker>(R.id.np3)
+        val np4 = findViewById<NumberPicker>(R.id.np4)
+        val btSetNum = findViewById<Button>(R.id.btSetNum)
+        val btUnlock = findViewById<Button>(R.id.btUnlock)
+        setNp(np1)
+        setNp(np2)
+        setNp(np3)
+        setNp(np4)
+        btSetNum.setOnClickListener {
+            val  nums="${np1.value}${np2.value}${np3.value}${np4.value}"
+            Toast.makeText(this,nums,Toast.LENGTH_LONG).show()
+        }
+        btUnlock.setOnClickListener {
+
+        }
+
+    }
+}
