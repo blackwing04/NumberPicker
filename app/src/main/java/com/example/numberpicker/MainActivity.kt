f9371742c2706264751e4db9d026969bc8c4e15f
blackwing <hh4654075hh@gmail.com>
2022-11-22 19:39:01 +0800

    密碼鎖完成

--- app/src/main/java/com/example/numberpicker/MainActivity.kt
+++ app/src/main/java/com/example/numberpicker/MainActivity.kt
@@ -5,32 +5,53 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.Toast
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+    private val btSetNum by lazy {findViewById<Button>(R.id.btSetNum)}
+    private val btUnlock by lazy {findViewById<Button>(R.id.btUnlock)}
+    private val np1 by lazy {findViewById<NumberPicker>(R.id.np1)}
+    private val np2 by lazy {findViewById<NumberPicker>(R.id.np2)}
+    private val np3 by lazy {findViewById<NumberPicker>(R.id.np3)}
+    private val np4 by lazy {findViewById<NumberPicker>(R.id.np4)}
+    private var passwd = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fun setNp(np:NumberPicker){
-            np.minValue=0
-            np.maxValue=9
-        }
-        val np1 = findViewById<NumberPicker>(R.id.np1)
-        val np2 = findViewById<NumberPicker>(R.id.np2)
-        val np3 = findViewById<NumberPicker>(R.id.np3)
-        val np4 = findViewById<NumberPicker>(R.id.np4)
-        val btSetNum = findViewById<Button>(R.id.btSetNum)
-        val btUnlock = findViewById<Button>(R.id.btUnlock)
         setNp(np1)
         setNp(np2)
         setNp(np3)
         setNp(np4)
+        setupUiListener()
+
+    }
+
+    fun setNp(np:NumberPicker){
+        np.minValue=0
+        np.maxValue=9
+    }
+
+    fun showMsg(msg:String){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+    }
+
+    fun setupUiListener()
+    {
         btSetNum.setOnClickListener {
-            val nums="${np1.value}${np2.value}${np3.value}${np4.value}"
-            Toast.makeText(this,nums,Toast.LENGTH_LONG).show()
+            passwd=np1.value*1000 + np2.value*100 + np3.value*10 + np4.value
+            showMsg(getString(R.string.passwd_set))
         }
         btUnlock.setOnClickListener {
-
+            if (passwd <0){
+                showMsg(getString(R.string.passwd_not_set))
+                return@setOnClickListener
+            }
+            val num =np1.value*1000 + np2.value*100 + np3.value*10 + np4.value
+            if (num == passwd){
+                showMsg(getString(R.string.correct))
+            }
+            else  showMsg(getString(R.string.error))
         }
-
     }
 }
